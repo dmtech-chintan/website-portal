a4fb8673cca9de80e2b2f7e37723a96989778658
Dipak Patil <chintan@dmtech.me>
2023-12-22 19:06:44 +0530

    design created

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#topheader .navbar-nav a").on("click", function () {
+    $("#topheader .navbar-nav").find("li.active-header").removeClass("active-header");
+    $(this).parent("li").addClass("active-header");
+  });
+});
+
+$(function () {
+  $("#header").load("header.html");
+  $("#footer").load("footer.html");
+});
